0f30af51a0b19d1ede4b4ffe9085d51151b3fca0
cspnorth <cate@roguenotion.com>
2017-11-12 17:02:34 -0600

    styled case studies archive page

--- wp-content/themes/accelerate-theme-child/archive-case_studies.php
+++ wp-content/themes/accelerate-theme-child/archive-case_studies.php
@@ -8,13 +8,33 @@
  */
 
 get_header(); ?>
-<p> os tjos wprlogn</p>
+
 	<div id="primary" class="site-content">
 		<div class="main-content" role="main">
-			<h4>Is this page working?</h4>
-			<?php while ( have_posts() ) : the_post(); ?>
-			<p>is this working</p>
-				<?php the_content(); ?>
+			<?php while ( have_posts() ) : the_post();
+				$services = get_field('services');
+				$client = get_field('client');
+				$link = get_field('site_link');
+				$image_1 = get_field('image_1');
+				$size = "full"; 
+			?>
+
+			<article class="case-study">
+				<div class="case-study-row">
+					<aside class="case-study-sidebar">
+						<h2><?php the_title(); ?></h2>
+						<h5><span><?php echo $services; ?></span></h5>
+						<?php the_content(); ?>
+						<p><a href="<?php echo $link; ?>">View project &raquo;</a></p>
+					</aside>
+					<div class="case-study-images">
+						<?php if($image_1) {
+							echo wp_get_attachment_image($image_1, $size );
+						} ?>
+					</div>
+				</div>	
+			</article>
+
 			<?php endwhile; // end of the loop. ?>
 		
 		</div><!-- .main-content -->
